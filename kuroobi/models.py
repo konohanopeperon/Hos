53d9a765f08f7d585f6b91d2afaa24aa2b08fe3a
pepe <211078@std.hi-joho.ac.jp>
2024-05-23 12:23:03 +0900

    up

---
+++ kuroobi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    empid = models.CharField(max_length=8, primary_key=True)
+    empfname = models.CharField(max_length=64)
+    emplname = models.CharField(max_length=64)
+    emppasswd = models.CharField(max_length=256)
+    emprole = models.IntegerField()
